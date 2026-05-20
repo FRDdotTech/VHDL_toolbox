d3569eefbca0fe219b4030bb906e6369e5c8e31c
Your Name <your.email@example.com>
2026-05-20 16:12:14 +0200

    Q1 valid WIP on Q and Q uart

--- compt_e.vhd
+++ compt_e.vhd
@@ -9,7 +9,9 @@ entity compt_e is
 	generic(start, stop : in std_logic_vector(31 downto 0) := x"00000000");
 	port(clk, En, reload: in std_logic;
 			step: in std_logic_vector(2 downto 0);
-			val_out: out std_logic_vector(31 downto 0));
+			val_out: out std_logic_vector(31 downto 0);
+			overflow : out std_logic);
+			
 end entity;
 
 architecture compt_a of compt_e is
@@ -23,12 +25,14 @@ begin
 		if reload = '1'then
 			count := x"00000000";
 			count := count + step;
+			overflow <= '0';
 		end if;
 		
 		if rst_flag = '1'then
 			count := start;
 			output := start;
 			rst_flag := '0';
+			overflow <= '0';
 		end if;
 		
 		if En = '1' then
@@ -36,6 +40,10 @@ begin
 			count := count + step;
 		end if;
 	
+		if count > x"f" then
+			overflow <= '1';
+		end if;
+		
 		if count > stop then
 			rst_flag := '1';
 		end if;
--- seven_seg_e.vhd
+++ seven_seg_e.vhd
@@ -9,7 +9,7 @@ use IEEE.numeric_std.all;
 
 
 entity seven_seg_e is
-	port(A: in std_logic_vector(7 downto 0);
+	port(A: in std_logic_vector(3 downto 0);
 			S: out std_logic_vector(6 downto 0));
 end entity;
 
@@ -18,29 +18,22 @@ begin
 process(A)
 begin
 	case A is 
-		when X"00"	=> S <= b"1000000";		-- 0
-		when X"01"	=> S <= b"1111001";		-- 1
-		when X"02"	=> S <= b"0100100";		-- 2
-		when X"03"	=> S <= b"0110000";		-- 3
-		when X"04"	=> S <= b"0011001";		-- 4
-		when X"05"	=> S <= b"0010010";		-- 5
-		when X"06"	=> S <= b"0000010";		-- 6
-		when X"07"	=> S <= b"1111000";		-- 7
-		when X"08"	=> S <= b"0000000";		-- 8
-		when X"09"	=> S <= b"0010000";		-- 9
-		when X"0A"	=> S <= b"0001000";		-- A
-		when X"0B"	=> S <= b"0000011";		-- B
-		when X"0C"	=> S <= b"1000110";		-- C
-		when X"0D"	=> S <= b"0100001";		-- D
-		when X"0E"	=> S <= b"0000110";		-- E
-		when X"0F"	=> S <= b"0001110";		-- F
-		when X"10"	=> S <= b"0111111";		-- mid
-		when X"11"	=> S <= b"1011111";		-- top_left
-		when X"12"	=> S <= b"1101111";		-- bottom_left
-		when X"13"	=> S <= b"1110111";		-- bottom
-		when X"14"	=> S <= b"1111011";		-- bottom_right
-		when X"15"	=> S <= b"1111101";		-- top_right
-		when X"16"	=> S <= b"1111110";		-- top
+		when X"0"	=> S <= b"1000000";		-- 0
+		when X"1"	=> S <= b"1111001";		-- 1
+		when X"2"	=> S <= b"0100100";		-- 2
+		when X"3"	=> S <= b"0110000";		-- 3
+		when X"4"	=> S <= b"0011001";		-- 4
+		when X"5"	=> S <= b"0010010";		-- 5
+		when X"6"	=> S <= b"0000010";		-- 6
+		when X"7"	=> S <= b"1111000";		-- 7
+		when X"8"	=> S <= b"0000000";		-- 8
+		when X"9"	=> S <= b"0010000";		-- 9
+		when X"A"	=> S <= b"0001000";		-- A
+		when X"B"	=> S <= b"0000011";		-- B
+		when X"C"	=> S <= b"1000110";		-- C
+		when X"D"	=> S <= b"0100001";		-- D
+		when X"E"	=> S <= b"0000110";		-- E
+		when X"F"	=> S <= b"0001110";		-- F
 		when others => S <= b"1111111";		-- NONE
 	end case;
 	end process;
